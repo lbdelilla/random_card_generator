7e92c6be3336e0a0d88f08a1756dc19cdcf87437
Lucía Belén <lbdelilla@gmail.com>
2023-05-26 16:59:48 +0000

    feat: created a random card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suit = ["♥", "♦", "♠", "♣"];
+const value = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let upsymbol = document.querySelector(".up");
+let downsymbol = document.querySelector(".down");
+let number = document.querySelector(".number");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  function generateCard() {
+    let pickSuit = suit[Math.floor(Math.random() * (suit.length - 1))];
+    let pickValue = value[Math.floor(Math.random() * (value.length - 1))];
+    upsymbol.innerHTML = pickSuit;
+    downsymbol.innerHTML = pickSuit;
+    number.innerHTML = pickValue;
+
+    pickSuit === "♥" || pickSuit === "♦"
+      ? ((upsymbol.style.color = "red"), (downsymbol.style.color = "red"))
+      : ((upsymbol.style.color = "black"), (downsymbol.style.color = "black"));
+  }
+  document.querySelector(".btn").addEventListener("click", function() {
+    generateCard();
+  });
+  setInterval(generateCard, 10000);
 };
